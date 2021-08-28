f350cc311d2fccd1eb9c389e681f62d1e6119653
SteFowles-White <fowleswhite@gmail.com>
2021-08-28 14:02:22 +0100

    completed an accesbility report using lighthouse, made a few form amends - there is an issue text to background colour contrast that neeeds raising

--- basket-task/src/App.js
+++ basket-task/src/App.js
@@ -10,35 +10,36 @@ function App() {
         <div className="row fs-height justify-content-center align-items-center">
           <section className="card__basket__container">
             <div className="card card__top pt-2 pb-3 p-4 background--off-white">
-              <form className="card-body pb-0 pt-0">
+            {/* assuming data will be sent to an API to update user account */}
+              <form id="basketForm" action="" className="card-body pb-0 pt-0">
                 <div className="row pt-3 pb-3 card__item">
                   <div className="col-sm-7 justify-content-between d-flex align-items-center ps-0 pe-0">
-                      <label for="product-id-1" className="">Mountain Dew</label>
+                      <label htmlFor="product-id-1" className="">Mountain Dew</label>
                       <input type="text" className="card__total_quantity text-center" id="product-id-1" aria-describedby="product-id-1"/>
                   </div> 
                   <div className="col-sm-5 pe-0 ps-0 pt-2 pt-sm-0 d-flex align-items-center justify-content-sm-end justify-content-between">
                     <p className="pe-3 card__total_value">$19.33</p>
-                    <button className="btn__cancel pe-sm-0"><FontAwesomeIcon icon={ faTimes } /></button>
+                    <button type="button" aria-label="Remove items" className="btn__cancel pe-sm-0"><FontAwesomeIcon icon={ faTimes } /></button>
                   </div>  
                 </div>       
                 <div className="row pt-3 pb-3 card__item">
                   <div className="col-sm-7 justify-content-between d-flex align-items-center ps-0 pe-0">
-                      <label for="product-id-2" className="">Mountain Dew</label>
+                      <label htmlFor="product-id-2" className="">Mountain Dew</label>
                       <input type="text" className="card__total_quantity text-center" id="product-id-2" aria-describedby="product-id-2"/>
                   </div> 
                   <div className="col-sm-5 pe-0 ps-0 pt-2 pt-sm-0 d-flex align-items-center justify-content-sm-end justify-content-between">
                     <p className="pe-3 card__total_value">$19.33</p>
-                    <button className="btn__cancel pe-sm-0"><FontAwesomeIcon icon={ faTimes } /></button>
+                    <button type="button" aria-label="Remove items" className="btn__cancel pe-sm-0"><FontAwesomeIcon icon={ faTimes } /></button>
                   </div>  
                 </div>       
                 <div className="row pt-3 pb-3">
                   <div className="col-sm-7 justify-content-between d-flex align-items-center ps-0 pe-0">
-                      <label for="product-id-3" className="">Mountain Dew</label>
+                      <label htmlFor="product-id-3" className="">Mountain Dew</label>
                       <input type="text" className="card__total_quantity text-center" id="product-id-3" aria-describedby="product-id-3"/>
                   </div> 
                   <div className="col-sm-5 pe-0 ps-0 pt-2 pt-sm-0 d-flex align-items-center justify-content-sm-end justify-content-between">
                     <p className="pe-3 card__total_value">$19.33</p>
-                    <button className="btn__cancel pe-sm-0 pb-0"><FontAwesomeIcon icon={ faTimes } /></button>
+                    <button type="button" aria-label="Remove items" className="btn__cancel pe-sm-0 pb-0"><FontAwesomeIcon icon={ faTimes } /></button>
                   </div>  
                 </div>       
               </form>     
@@ -49,8 +50,8 @@ function App() {
                     <p className="mb-0 pt-2 pb-2 card__total">$ 34.54</p>
                 </div> 
                 <div className="col-sm-7 card__totals__container">
-                  <button className="pe-3 btn__clear">Clear</button>
-                  <button className="btn__blue">Check Out  <FontAwesomeIcon icon={ faGreaterThan } /></button>
+                  <button type="button" className="pe-3 btn__clear">Clear</button>
+                  <button form="basketForm" type="submit" value="submit" className="btn__blue">Check Out  <FontAwesomeIcon icon={ faGreaterThan } /></button>
                 </div>
               </div>
             </div>
